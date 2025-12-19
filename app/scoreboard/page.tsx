207d4bf545e5fc6413a2d2f459f14627f81f881b
Grayson <78488596+GraysonDeiley@users.noreply.github.com>
2025-12-19 11:47:24 -0500

    Feature/scoreboard (#20)
    * added scoreboard working on the styling
    
    * scoreboard finalized

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -1,7 +1,23 @@
+import ScoreboardBoard from "../features/scoreboard/board"
+
+const pageStyle: React.CSSProperties = {
+    minHeight: "100vh",
+    width: "100vw",
+    // backgroundImage: "url('https://i.imgur.com/EOcFqy1.png')",
+    backgroundImage: "url('https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWo2ZnJvNzdxMnNrd3NyajE1bzNnbG1lZXFqMHA0eWppcmFxbGRtNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dpu95OTQ9rDPi/giphy.gif')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+};
 
 function page() {
     return (
-        <div>page</div>
+        <div style={pageStyle}>
+            <ScoreboardBoard />
+        </div>
     )
 }
 
